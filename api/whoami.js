37f2a5d88392e036692407ba3cea7e96f4d74467
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 21:54:49 -0700

    Update whoami.js

--- api/whoami.js
+++ api/whoami.js
@@ -1,1 +1,39 @@
-res.send(env);
+var v = req.body.ip, v_space = req.body.space;
+/*
+function isIp(ip) {
+    var arrIp = ip.split(".");
+    if (arrIp.length !== 4) return "Invalid IP";
+    for (let oct of arrIp) {
+        if ( isNaN(oct) || Number(oct) < 0 || Number(oct) > 255)
+            return false;
+    }
+    return true;
+}
+*/
+
+var CP = new pkg.crowdProcess();
+var _f = {};
+
+for (var i = 0; i < v.length; i++) {
+	_f[i] = (function(i) {
+		return function(cbk) {
+			pkg.request({
+				url: 'http://'+v[i]+'/checkip/',
+				headers: {
+				    "content-type": "application/json"
+				}
+			    }, function (error, resp, body) { 
+              cbk('NIU');
+			   });
+		}	
+	})(i);
+}
+
+
+CP.parallel(
+	_f,
+	function(data) {
+		res.send(data);
+	},
+	30000
+);
