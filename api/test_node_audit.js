ffaa5e357b0e9aa9e77cfb47512d993c04a14e76
b2blosangeles <b2blosangeles@gmail.com>
2017-11-04 12:11:24 -0700

    Update test_node_audit.js

--- api/test_node_audit.js
+++ api/test_node_audit.js
@@ -41,10 +41,10 @@ _f['D2'] = function(cbk) {
 						else {
 							var audit = [];
 							try { audit = JSON.parse(recs[i].audit); } catch(e) {}
-							audit[audit.length+1] = new Date().getTime();
+							// audit[audit.length+1] = new Date().getTime();
 							var str = "UPDATE `cloud_node` SET `audit` = '" + JSON.stringify(audit) + 
 							    "' WHERE node_ip = '" + ip + "'";
-							cbk1('str3');
+							cbk1('str6');
 						}	
 						/*
 						var v = [];
