74f4525108f2163e58c532db8c4bb849823d11e3
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 15:33:25 -0800

    Update reboot_all.js

--- api/reboot_all.js
+++ api/reboot_all.js
@@ -18,7 +18,46 @@ _f['D1'] = function(cbk) {
 		}
 	});	
 }
-
+_f['D2'] = function(cbk) {
+	if  (CP.data.D1 == false) {  cbk(false); return true; }
+	
+	var CP1 = new crowdProcess();
+	var _f1 = {}, recs = CP.data.D1;	
+	for (var i = 0; i < recs.length; i++) {
+		/* --- git pull code */
+		_f1['PGIT_'+i] = (function(i) {
+			return function(cbk1) {
+				var ip = recs[i].node_ip;
+				var delay = randomInt(0,300) * 10;
+				setTimeout(
+					function() {
+						request({
+							url: 'http://'+ ip +'/api/admin.api',
+							method: 'POST',
+							headers: {
+							    "content-type": "application/json"
+							},
+							form: {opt:'git_all_pull'},
+							timeout: 5900
+						    }, function (error, resp, body) { 
+							console.log('Called ' + 'http://'+ ip +'/api/admin.api');
+							if (error) console.log(error.message);
+							if (body) console.log(body);
+							cbk1(true);
+						   });	
+					}, delay
+				      );						
+			}
+		})(i);		
+	}
+	
+	CP1.parallel(
+		_f1,
+		function(data) {
+			cbk(data);
+		}, 10000
+	);	
+}
 CP.parallel(
 	_f,
 	function(data) {
