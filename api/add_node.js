01ed1e6abc4c2514ab2bba4c2067975058009b45
b2blosangeles <b2blosangeles@gmail.com>
2018-03-14 11:03:40 -0700

    Update add_node.js

--- api/add_node.js
+++ api/add_node.js
@@ -1,6 +1,7 @@
 var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql');
 // Shusiou Node will call this API to add himself into  
-var cfg0 = require(env.config_path + '/db.json');
+var config = require(env.config_path + '/config.json');
+var cfg0 = config.db;
 
 var v = req.body.ip, space = req.body.space;
 if (!v) {
